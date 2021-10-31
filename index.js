e77e28f42772f825ee742944a87bd0968c468ae4
Seamus Donnellan <42863425+donnellan0007@users.noreply.github.com>
2021-10-31 18:47:10 +1100

    Debugging Phase 2 - QS --> QSA

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ selector.style.height = activeTab.innerHeight + "px";
 tab.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(e.target)
-  tab.querySelector("a").classList.remove("active");
+  tab.querySelectorAll("a").classList.remove("active");
   e.target.classList.add("active");
   var activeWidth = e.target.innerWidth;
   var itemPosition = {
