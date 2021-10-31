004f7b7a5d59ca510648b1ce8c061ed24c62e3ec
Seamus Donnellan <42863425+donnellan0007@users.noreply.github.com>
2021-10-31 18:48:25 +1100

    QSA --> EBTN

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ selector.style.height = activeTab.innerHeight + "px";
 tab.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(e.target)
-  tab.querySelectorAll("a").classList.remove("active");
+  tab.getElementsByTagName("a").classList.remove("active");
   e.target.classList.add("active");
   var activeWidth = e.target.innerWidth;
   var itemPosition = {
